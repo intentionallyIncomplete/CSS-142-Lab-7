4ce3eff9d79aa9a6c51b39557c590309418a09ca
Ian Bryan <ianbryanz@gmail.com>
2018-05-14 15:34:03 -0700

    More Methods Added
    Added two methods that have been tested and commented.

--- src/CapitolizeOneString.java
+++ src/CapitolizeOneString.java
@@ -9,18 +9,68 @@ import java.util.*;
  */
 public class CapitolizeOneString {
 
+	private static String[] newStrings;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner keyboardInput = new Scanner(System.in);
-        String userGivenString;
-        System.out.println("Enter a sentence");
-        userGivenString = keyboardInput.nextLine();
-        printCapitalized(userGivenString);
-    }
+		/*Scanner object for getting user input*/
+		Scanner keyboardInput = new Scanner(System.in);
+		String userGivenString;
+		System.out.println("Enter a sentence");
+		userGivenString = keyboardInput.nextLine();
 
-    static void printCapitalized(String userGivenString) {
-        System.out.println("Words: " + userGivenString);
+		/*
+		 * Static method calls
+		 * printCaptialized(arg0){} --> Takes in a sentence provide by the user and returns it with each first letter
+		 * capitalized. 
+		 * 
+		 * nameInitials(arg0){} --> Takes in a string and returns the initials
+		 * */
 
-    }
+		//printCapitalized(userGivenString);
+		
+		//nameInitials(userGivenString);
+	}
+
+	/**
+	* This method will take in the String value that represents
+	* the user input obtained from Scanner and 
+	* split the words by the empty string (space) delimiter.
+	* The split() method returns an array that is iterated over
+	* and each first letter is converted to upper case and appended
+	* to the rest of the string starting at the next position.
+	*
+	* @param userGivenString represents the input from the user that was obtained
+	* using the Scanner object 
+	* @see Scanner in main method
+	* */
+	static void printCapitalized(String userGivenString) {
+
+		newStrings = userGivenString.split(" ");
+		System.out.print("New Sentence: ");
+		for (int i=0; i<newStrings.length;i++) {
+			System.out.print(newStrings[i].toUpperCase().charAt(0) + newStrings[i].substring(1) + " ");
+		}
+		System.out.println("\nOld Sentence: " + userGivenString);
+	}
+	
+	/**
+	* This method here will take in a string value provided by the user
+	* obtained by Scanner and split it using the split() method and a 
+	* space as a delimiter. Once the strings are split, they are added
+	* to an array and iterated over. While iterating, the first letters
+	* are printed to the console capitalized and have a "." appended to
+	* the end. 
+	*
+	* @see printCaptitalized()
+	* */
+	static void nameInitials(String userGivenString) {
+		newStrings = userGivenString.split(" ");
+		
+		for(int i=0;i<newStrings.length;i++) {
+			System.out.print(newStrings[i].toUpperCase().charAt(0) + ".");
+		}
+	}
+	
+	
 }
